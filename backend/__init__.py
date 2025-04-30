b219c10f3f045a6727e45c0fd5e78ba3adc85eaa
Oliver Hiltz-Perron <olivert831@gmail.com>
2025-04-30 15:55:48 -0400

    readded gitignore

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/__init__.py
-
-from .llamaparse_service import LlamaParseService
-from .markdown_fixer_service import MarkdownFixerService
-from .proposition_extractor import PropositionExtractionService
-
-__all__ = ['LlamaParseService', 'MarkdownFixerService', 'PropositionExtractionService']
